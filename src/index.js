1179f75229158cb8f4d62536f21bf219adcbda40
Isaque Tanno <tannoisaque@gmail.com>
2026-09-23 20:05:14 -0300

    Estrutura inicial do servidor close #17

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+
+app.listen(3000,() => {
+    console.log("Servidor ativo na porta 3000")
+});
+
+app.get("/",(req, res) => {
+    res.send("Mensagem Teste Servidor");
+    // res.sendFile(path.join(__dirname, '../dashboard.html'));
+});
